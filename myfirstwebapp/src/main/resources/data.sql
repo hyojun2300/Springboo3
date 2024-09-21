6532e1c8d4a9cdc186de572d3752f1d3e195bf02
hyojun2300 <127063236+hyojun2300@users.noreply.github.com>
2024-09-21 21:00:57 +0900

    Add files via upload

---
+++ myfirstwebapp/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+ insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+ values(10001, 'ranga', 'GET AWS Certified', CURRENT_DATE(), false);
+ 
+ insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+ values(10002, 'ranga', 'GET Azure Certified', CURRENT_DATE(), false);
+ 
+ insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+ values(10003, 'ranga', 'GET GCP Certified', CURRENT_DATE(), false);
+ 
+ insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+ values(10004, 'ranga', 'GET devOps Certified', CURRENT_DATE(), false);
---
+++ myfirstwebapp/target/classes/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -0,0 +1,11 @@
+<%@ include file="common/header.jspf" %>
+<%@ include file="common/navigation.jspf" %>
+	<div class="container">
+		<h1>Login</h1>
+		<pre>${errorMessage}</pre>
+		<form method="post">
+			Name: <input type="text" name="name"> PassWord: <input
+				type="password" name="password"> <input type="submit">
+		</form>
+	</div>
+<%@ include file="common/footer.jspf" %>
